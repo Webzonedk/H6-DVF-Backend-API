11123ffa14b6192b7f1e7bb95c252e6eef8f4a3b
Webzonedk <61316551+Webzonedk@users.noreply.github.com>
2024-04-24 14:40:51 +0200

    refactoring CrudFileRepository and DataService

---
+++ DVF-API/SharedLib/Dtos/BinaryDataFromFileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DVF_API.SharedLib.Dtos
+{
+    public class BinaryDataFromFileDto
+    {
+        public string Coordinates { get; set; }
+        public string YearDate { get; set; }
+        public byte[] BinaryWeatherData { get; set; }
+    }
+}
