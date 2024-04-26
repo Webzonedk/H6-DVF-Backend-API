9a9e01714badae75aeca1a1f3e442f776ebae980
Webzonedk <61316551+Webzonedk@users.noreply.github.com>
2024-04-26 18:57:11 +0200

    XP 001

--- DVF-API/Services/ServiceImplementation/DeveloperService.cs
+++ DVF-API/Services/ServiceImplementation/DeveloperService.cs
@@ -121,7 +121,7 @@ namespace DVF_API.Services.ServiceImplementation
                 }
                 if (createFiles)
                 {
-                   var result = MapDataSaveToStorageDtoToByteArray(saveToStorageDto);
+                    var result = MapDataSaveToStorageDtoToByteArray(saveToStorageDto);
                     // await _historicWeatherDataRepository.SaveDataToFileAsync(saveToStorageDto, _baseDirectory);
 
                 }
@@ -138,52 +138,55 @@ namespace DVF_API.Services.ServiceImplementation
         {
             ConcurrentBag<HistoricWeatherDataToFileDto> historicWeatherDataToFileDtos = new ConcurrentBag<HistoricWeatherDataToFileDto>();
 
-            var groupedData = historicWeatherDataToFileDtos.GroupBy(dto => MixedYearDateTimeSplitter(dto.Time));
-            historicWeatherDataToFileDtos = new ConcurrentBag<HistoricWeatherDataToFileDto>();
 
-            foreach (var group in groupedData)
+            try
             {
-                try
+                Parallel.ForEach(_saveToStorageDto, data =>
                 {
-                    Parallel.ForEach(_saveToStorageDto, data =>
+                    for (int index = 0; index < data.HistoricWeatherData.Hourly.Time.Length; index++)
                     {
-                        for (int index = 0; index < data.HistoricWeatherData.Hourly.Time.Length; index++)
+                        try
                         {
-                            try
+                            HistoricWeatherDataToFileDto historicWeatherDataToFileDto = new HistoricWeatherDataToFileDto
                             {
-                                HistoricWeatherDataToFileDto historicWeatherDataToFileDto = new HistoricWeatherDataToFileDto
-                                {
-                                    Id = data.LocationId,
-                                    Latitude = ConvertCoordinate(data.Latitude),
-                                    Longitude = ConvertCoordinate(data.Longitude),
-                                    Time = ConvertDateTimeToFloatInternal(data.HistoricWeatherData.Hourly.Time[index]),
-                                    Temperature_2m = data.HistoricWeatherData.Hourly.Temperature_2m[index],
-                                    Relative_Humidity_2m = data.HistoricWeatherData.Hourly.Relative_Humidity_2m[index],
-                                    Rain = data.HistoricWeatherData.Hourly.Rain[index],
-                                    Wind_Speed_10m = data.HistoricWeatherData.Hourly.Wind_Speed_10m[index],
-                                    Wind_Direction_10m = data.HistoricWeatherData.Hourly.Wind_Direction_10m[index],
-                                    Wind_Gusts_10m = data.HistoricWeatherData.Hourly.Wind_Gusts_10m[index],
-                                    Global_Tilted_Irradiance_Instant = data.HistoricWeatherData.Hourly.Global_Tilted_Irradiance_Instant[index]
-                                };
-                                historicWeatherDataToFileDtos.Add(historicWeatherDataToFileDto);
-                            }
-                            catch (Exception ex)
-                            {
-                                Debug.WriteLine($"{ex.Message}");
-                            }
+                                Id = data.LocationId,
+                                Latitude = ConvertCoordinate(data.Latitude),
+                                Longitude = ConvertCoordinate(data.Longitude),
+                                Time = ConvertDateTimeToFloatInternal(data.HistoricWeatherData.Hourly.Time[index]),
+                                Temperature_2m = data.HistoricWeatherData.Hourly.Temperature_2m[index],
+                                Relative_Humidity_2m = data.HistoricWeatherData.Hourly.Relative_Humidity_2m[index],
+                                Rain = data.HistoricWeatherData.Hourly.Rain[index],
+                                Wind_Speed_10m = data.HistoricWeatherData.Hourly.Wind_Speed_10m[index],
+                                Wind_Direction_10m = data.HistoricWeatherData.Hourly.Wind_Direction_10m[index],
+                                Wind_Gusts_10m = data.HistoricWeatherData.Hourly.Wind_Gusts_10m[index],
+                                Global_Tilted_Irradiance_Instant = data.HistoricWeatherData.Hourly.Global_Tilted_Irradiance_Instant[index]
+                            };
+                            historicWeatherDataToFileDtos.Add(historicWeatherDataToFileDto);
                         }
-                    });
-                    //Vi mangler at gruppere ud fra dato Vi mangler årstal og dato struktur for at kunne lave filerne mindre og smide dem afsted løbende.
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine($"{ex.Message}");
+                        }
+                    }
+                });
+
+                //Vi mangler at gruppere ud fra dato Vi mangler årstal og dato struktur for at kunne lave filerne mindre og smide dem afsted løbende.
+
+                var groupedData = historicWeatherDataToFileDtos.GroupBy(dto => MixedYearDateTimeSplitter(dto.Time));
+                historicWeatherDataToFileDtos = new ConcurrentBag<HistoricWeatherDataToFileDto>();
+
+                foreach (var group in groupedData)
+                {
 
                     var orderedList = historicWeatherDataToFileDtos.OrderBy(x => x.Id).ThenBy(x => x.Time).ToList();
                     return ConvertModelToBytesArray(orderedList);
-
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine($"{ex.Message}");
                 }
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"{ex.Message}");
+            }
+
             return null;
         }
 
@@ -191,11 +194,11 @@ namespace DVF_API.Services.ServiceImplementation
         {
             using (var memoryStream = new MemoryStream())
             {
-                var options = new JsonSerializerOptions { WriteIndented = true };
-                using (var writer = new Utf8JsonWriter(memoryStream, options))
-                {
-                    JsonSerializer.Serialize(writer, orderedList);
-                }
+                //var options = new JsonSerializerOptions { WriteIndented = true };
+                //using (var writer = new Utf8JsonWriter(memoryStream, options))
+                //{
+                //    JsonSerializer.Serialize(writer, orderedList);
+                //}
                 orderedList.Clear();
                 return memoryStream.ToArray();
             }
