949284b3ffe3ee5b1a156e98e56bb9be76b89877
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2024-04-30 20:01:23 +0200

    new merge needed

--- DVF-API/Domain/BusinessLogic/UtilityManager.cs
+++ DVF-API/Domain/BusinessLogic/UtilityManager.cs
@@ -205,6 +205,7 @@ namespace DVF_API.Domain.BusinessLogic
         /// <returns>a string representing the time measurement in a human-readable format.</returns>
         public string ConvertTimeMeasurementToFormat(float time)
         {
+
             if (time < 1000)
             {
                 return $"{time.ToString("0.##")} ms";
